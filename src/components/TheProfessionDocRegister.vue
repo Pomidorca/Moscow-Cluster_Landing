77af1f9b2ebf203257439f89c4b6a75c35944892
Maritxuak <dima.dmitrii.2017@mail.ru>
2025-01-30 17:19:02 +0300

    update

--- src/components/TheProfessionDocRegister.vue
+++ src/components/TheProfessionDocRegister.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <slot class="text-4xl font-medium text-PrimaryOrange2"></slot>
-        <div class="grid grid-cols-4 gap-6 mt-10 max-lg:grid-cols-1">
-            <button class="px-5 py-8 bg-PrimaryOrange2 rounded-2xl w-full flex flex-col items-start"
-                v-for="item in list" :ket="item.id">
+        <div class="grid grid-cols-3 gap-6 mt-10 max-lg:grid-cols-1">
+            <a class="px-5 py-8 bg-PrimaryOrange2 rounded-2xl w-full flex flex-col items-start"
+                v-for="item in list" :key="item.id" :href="item.path" download>
                 <img :src="item.image" alt="Icons" loading="lazy">
                 <span class="mt-4 text-Background text-2xl font-medium">{{ item.title }}</span>
-            </button>
+            </a>
         </div>
     </div>
 </template>
@@ -16,22 +16,20 @@ const list = [
     {
         id: 1,
         image: '/image/main/18.svg',
-        title: 'Устав'
+        title: 'Устав',
+        path: '/documents/UchDokum_f913a815c0a84ee09e0fa092fac2acd4.pdf'
     },
     {
         id: 2,
         image: '/image/main/19.svg',
-        title: 'Свидетельство'
+        title: 'Свидетельство',
+        path: '/documents/SvidUchetUL_9aa4222e99724cda8e4b48a787d0a0f5.pdf'
     },
     {
         id: 3,
         image: '/image/main/20.svg',
-        title: 'ЕГРЮЛо создании'
-    },
-    {
-        id: 4,
-        image: '/image/main/21.svg',
-        title: 'ЕГРЮЛ об учете'
+        title: 'ЕГРЮЛо создании',
+        path: '/documents/VypZapEGRUL_750078bc24e347528b52aec849869ae6.pdf'
     }
 ]
 </script>
--- src/components/TheProfessionIntroduction.vue
+++ src/components/TheProfessionIntroduction.vue
@@ -5,17 +5,26 @@
             <div class="bg-SecondaryDark px-5 py-7 rounded-2xl w-full" v-for="item in list1" :key="item.id">
                 <div class="flex justify-between">
                     <img :src="item.image" alt="PDF" loading="lazy">
-                    <span class="flex items-center gap-x-4">0.2 MB <img src="/image/main/download.svg" alt=""></span>
+                    <a class="flex items-center gap-x-4" :href="item.path" download>0.2 MB <img src="/image/main/download.svg" alt=""></a>
                 </div>
                 <span class="text-xl text-PrimaryOrange3 font-semibold mt-10 block">{{ item.title }}</span>
             </div>
         </div>
+      <div class="grid grid-cols-3 gap-6 mt-10 max-lg:grid-cols-1">
+        <div class="bg-SecondaryDark px-5 py-7 rounded-2xl w-full" v-for="item in list3" :key="item.id">
+          <div class="flex justify-between">
+            <img :src="item.image" alt="PDF" loading="lazy">
+            <a class="flex items-center gap-x-4" :href="item.path" download>0.2 MB <img src="/image/main/download.svg" alt=""></a>
+          </div>
+          <span class="text-xl text-PrimaryOrange3 font-semibold mt-10 block">{{ item.title }}</span>
+        </div>
+      </div>
         <div class="grid grid-cols-2 gap-6 mt-6 max-md:grid-cols-1">
             <div class="bg-SecondaryDark px-5 py-7 rounded-2xl w-full" v-for="item in list2" :key="item.id">
                 <div class="flex justify-between">
                     <img :src="item.image" alt="PDF" loading="lazy">
-                    <button class="flex items-center gap-x-4">0.2 MB <img src="/image/main/download.svg"
-                            alt=""></button>
+                    <a class="flex items-center gap-x-4" :href="item.path" download> <img src="/image/main/download.svg"
+                            alt=""></a>
                 </div>
                 <span class="text-xl text-PrimaryOrange3 font-semibold mt-10 block">{{ item.title }}</span>
             </div>
@@ -28,29 +37,54 @@ const list1 = [
     {
         id: 1,
         image: '/image/main/22.svg',
-        title: 'Порядок вступления',
+        title: 'Договор МПК Пайщик',
+        path: '/documents/dogovor.pdf'
     },
     {
         id: 2,
         image: '/image/main/22.svg',
-        title: 'Порядок вступления',
+        title: 'Положение об имуществе МПК МИР паевый фонд',
+        path: '/documents/polozhenye_imyshestvo.pdf'
     },
     {
         id: 3,
         image: '/image/main/22.svg',
-        title: 'Порядок вступления',
+        title: 'Положение о членстве в МПК мир',
+        path: '/documents/polozhenye-4lenstvo.pdf'
     }
 ]
 const list2 = [
     {
         id: 1,
         image: '/image/main/22.svg',
-        title: 'Порядок вступления',
+        title: 'Положение о общем собрании МПК МИР',
+        path: '/documents/polozhenie-o-sobranii.pdf'
     },
     {
         id: 2,
         image: '/image/main/22.svg',
-        title: 'Порядок вступления',
+        title: 'Положение о взносах МПК МИР',
+        path: '/documents/polozhenye-o-vznosax.pdf'
     },
+]
+const list3 = [
+  {
+    id: 1,
+    image: '/image/main/22.svg',
+    title: 'Заявление о выходе из МПК МИР',
+    path: '/documents/zayvlenei-na-vixod.pdf'
+  },
+  {
+    id: 2,
+    image: '/image/main/22.svg',
+    title: 'Заявление о вступлении МПК МИР ЮЛ',
+    path: '/documents/zayva-vstyplenie-UP.pdf'
+  },
+  {
+    id: 3,
+    image: '/image/main/22.svg',
+    title: 'Заявление о вступлении МПК МИР ФЛ',
+    path: '/documents/zayva-na-vstyplenie-FL.pdf'
+  }
 ]
 </script>
