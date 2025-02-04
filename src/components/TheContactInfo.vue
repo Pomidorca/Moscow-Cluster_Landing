a4d4c7bb8f948be6b6ae4073ada2bb9ad3d878d9
Maritxuak <dima.dmitrii.2017@mail.ru>
2025-02-04 17:58:32 +0300

    update

--- src/components/TheContactInfo.vue
+++ src/components/TheContactInfo.vue
@@ -3,11 +3,11 @@
         <h2 class="text-2xl text-PrimaryOrange font-bold uppercase">Контакты</h2>
         <ul class="flex flex-col gap-y-4 mt-6">
             <li><a class="flex items-center gap-x-3 text-PrimaryOrange leading-5" href="tel:88005001905"><img
-                        src="/image/footer/Phone.svg" alt="Telephone">8 (800) -500 - 19 - 05</a></li>
+                        src="/image/footer/Phone.svg" alt="Telephone">+79094230110</a></li>
             <li><a class="flex items-center gap-x-3 text-PrimaryOrange leading-5" href="#"><img
-                        src="/image/footer/telegram.svg" alt="Telegram">7 (963) - 314 - 05 - 73</a></li>
+                        src="/image/footer/telegram.svg" alt="Telegram">https://t.me/mpkuniverse</a></li>
             <li><a class="flex items-center gap-x-3 text-PrimaryOrange leading-5" href="#"><img
-                        src="/image/footer/Letter.svg" alt="e-mail">post@</a>
+                        src="/image/footer/Letter.svg" alt="e-mail">universe.incc@gmail.com</a>
             </li>
         </ul>
     </div>
